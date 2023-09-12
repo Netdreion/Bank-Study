bf2f624353b420403d39e05cab2637505c35068e
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-12 19:21:12 -0400

    data added

---
+++ src/Components/Data.js
@@ -0,0 +1,10 @@
+const data = [
+  { id: 1, date: "2023-09-15", description: "Purchase", amount: 100 },
+  { id: 2, date: "2023-09-16", description: "Payment", amount: -50 },
+  { id: 3, date: "2023-09-17", description: "Withdrawal", amount: -30 },
+  { id: 4, date: "2023-09-18", description: "Deposit", amount: 200 },
+  { id: 5, date: "2023-09-19", description: "Purchase", amount: 75 },
+  // Add more transactions here...
+];
+
+export default data;
