ca91425fa66e07f23c58a53782c0f336cef62aa6
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-12 22:21:36 -0400

    TransactionFilters created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import TransactionHistory from "./components/TransactionHistory";
 import "./App.css";
+import TransactionFilters from "./components/TransactionFilters";
 
 function App() {
   return (
     <>
+      <TransactionFilters />
       <TransactionHistory />
     </>
   );
--- src/Components/TransactionFilters.jsx
+++ src/Components/TransactionFilters.jsx
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
 
 const TransactionFilters = () => {
-  return <div></div>;
+  const [date, setDate] = useState({
+    startDate: "",
+    endDate: "",
+  });
+
+  return (
+    <div>
+      <input
+        placeholder="start-date"
+        value={date.startDate}
+        onChange={(e) => {
+          setDate({ ...date, startDate: e.target.value });
+        }}
+      ></input>
+      <input
+        placeholder="end-date"
+        value={date.endDate}
+        onChange={(e) => setDate({ ...date, endDate: e.target.value })}
+      ></input>
+    </div>
+  );
 };
 
 export default TransactionFilters;
