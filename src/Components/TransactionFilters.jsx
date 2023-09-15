acc8d3ca03d8ebda4e84138e7d657633e8c2e77f
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-14 20:42:52 -0400

    css added

--- src/Components/TransactionFilters.jsx
+++ src/Components/TransactionFilters.jsx
@@ -16,42 +16,46 @@ const TransactionFilters = () => {
   };
 
   return (
-    <div>
-      <select
-        value={date.startDate}
-        onChange={(e) => setDate({ ...date, startDate: e.target.value })}
-      >
-        <option value="">Select Start Date</option>
-        <option value={"2023-09-15"}>2023-09-15</option>
-        <option value={"2023-09-16"}>2023-09-16</option>
-        <option value={"2023-09-17"}>2023-09-17</option>
-        <option value={"2023-09-18"}>2023-09-18</option>
-        <option value={"2023-09-19"}>2023-09-19</option>
-      </select>
-      <select
-        value={date.endDate}
-        onChange={(e) => setDate({ ...date, endDate: e.target.value })}
-      >
-        <option value="">Select End Date</option>
-        <option value={"2023-09-15"}>2023-09-15</option>
-        <option value={"2023-09-16"}>2023-09-16</option>
-        <option value={"2023-09-17"}>2023-09-17</option>
-        <option value={"2023-09-18"}>2023-09-18</option>
-        <option value={"2023-09-19"}>2023-09-19</option>
-      </select>
-      <button onClick={() => filter()}>Filter</button>
-      <div>
-        <ul>
-          {output.map((item, index) => (
-            <div key={index}>
-              <ul>
-                <li>{item.description}</li>
-                <li>{item.amount}</li>
-                <li>{item.date}</li>
-              </ul>
-            </div>
-          ))}
-        </ul>
+    <div className="container">
+      <div className="select-container">
+        <select
+          value={date.startDate}
+          onChange={(e) => setDate({ ...date, startDate: e.target.value })}
+        >
+          <option value="">Select Start Date</option>
+          <option value={"2023-09-15"}>2023-09-15</option>
+          <option value={"2023-09-16"}>2023-09-16</option>
+          <option value={"2023-09-17"}>2023-09-17</option>
+          <option value={"2023-09-18"}>2023-09-18</option>
+          <option value={"2023-09-19"}>2023-09-19</option>
+        </select>
+        <select
+          value={date.endDate}
+          onChange={(e) => setDate({ ...date, endDate: e.target.value })}
+        >
+          <option value="">Select End Date</option>
+          <option value={"2023-09-15"}>2023-09-15</option>
+          <option value={"2023-09-16"}>2023-09-16</option>
+          <option value={"2023-09-17"}>2023-09-17</option>
+          <option value={"2023-09-18"}>2023-09-18</option>
+          <option value={"2023-09-19"}>2023-09-19</option>
+        </select>
+        <div className="button-container">
+          <button onClick={() => filter()}>Filter</button>
+        </div>
+        <div className="results">
+          <ul>
+            {output.map((item, index) => (
+              <div key={index}>
+                <ul>
+                  <li>{item.description}</li>
+                  <li>{item.amount}</li>
+                  <li>{item.date}</li>
+                </ul>
+              </div>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
