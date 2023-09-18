816b2c19c5a6f3adef228c1d91389ac7800e854b
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-18 12:57:21 -0400

    bug fixed

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,29 +3,31 @@ import { useState } from "react";
 const Login = () => {
   const [user, setUser] = useState({
     userName: "",
-    password: null,
+    password: "",
   });
   const handleChange = (e) => {
-    if (user.userName.lenght > 5 && user.pasword.length > 5) {
-      return (
-        <div>
-          <p>please enter more then 5 charecter</p>
-        </div>
-      );
-    } else {
-      setUser({ userName: e.target.value, password: e.target.value });
-    }
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
   };
 
   return (
     <div>
       <input
+        name="userName"
         value={user.userName}
         placeholder="user-name"
         onChange={handleChange}
       ></input>
-      <input value="text" placeholder="user-name"></input>
-      <button>login</button>
+      <input
+        name="password"
+        value={user.password}
+        placeholder="password"
+        onChange={handleChange}
+      ></input>
+      <button>{user.userName.length < 5 ? "login" : "logout"}</button>
     </div>
   );
 };
