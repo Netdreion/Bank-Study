7cd5985acb454fb8517bec28a675ed0aef417984
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-21 20:12:11 -0400

    context comp created

--- src/Components/Context.jsx
+++ src/Components/Context.jsx
@@ -1,5 +1,15 @@
-function Context() {
-  return <></>;
-}
+const Context = () => {
+  return (
+    <>
+      <h3>welcome to Finovance</h3>
+      <table>
+        <th>one deposit account </th>
+        <tr>name</tr>
+        <tr>account number</tr>
+        <tr> acoount balance</tr>
+      </table>
+    </>
+  );
+};
 
 export default Context;
