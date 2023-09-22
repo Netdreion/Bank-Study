abd70ff2b4b8df5c1ee6c3bb04dfd6b4681f1943
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-21 20:08:15 -0400

    Dasborad comp created to host components

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
 import { useState } from "react";
-import Login from "./components/Login";
 
+import DashBoard from "./DashBoard";
 import "./App.css";
-import TransactionFilters from "./components/TransactionFilters";
 
 function App() {
-  const userSetup = {
-    userName: "oz",
-    password: "1234",
-  };
-  Object.freeze(userSetup);
-
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  // state for the user login form
-  const [loginForm, setLoginForm] = useState({
-    userName: "",
-    password: "",
-  });
-
-  // when clicked the button compare
-  // the userSetup object to the loginForm state object for password and userName
-  const handleLogin = () => {
-    if (
-      loginForm.userName === userSetup.userName &&
-      loginForm.password === userSetup.password
-    ) {
-      setLoggedIn(true);
-      alert("login succesfull");
-    } else {
-      alert("enter a valid username & password");
-      setLoggedIn(false);
-    }
-  };
   return (
     <>
-      <Login
-        setLoginForm={setLoginForm}
-        password={loginForm.password}
-        userName={loginForm.userName}
-        handleLogin={handleLogin}
-        // loginForm={loginForm}
-        // userSetup={userSetup}
-      />
-      {loggedIn && <TransactionFilters />}
+      <DashBoard />
     </>
   );
 }
