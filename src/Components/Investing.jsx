c23a268eb5f41300cf5fdb204249fe23baf61e27
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-10-17 21:54:48 -0400

    button added for on and off position

--- src/Components/Investing.jsx
+++ src/Components/Investing.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import { useState } from "react";
 
 const Investing = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div>
-      <h2>Welcome to fivoncae ivesting </h2>
-      <h4>Buying Power</h4>
-      <table>
-        <th></th>
-      </table>
+      <button
+        onClick={() => {
+          !show ? setShow(true) : setShow(!show);
+        }}
+      >
+        Investing
+      </button>
+      {show && (
+        <div>
+          <h2>Welcome to fivoncae investing</h2>
+          <h4>Buying Power</h4>
+          <table>
+            <tbody>
+              <tr>
+                <th>Stocks</th>
+                <td>hfhfhf</td>
+              </tr>
+              <tr>
+                <th>Stocks</th>
+                <td>hfhfhf</td>
+              </tr>
+              <tr>
+                <th>Stocks</th>
+                <td>hfhfhf</td>
+              </tr>
+              <tr>
+                <th>Stocks</th>
+                <td>hfhfhf</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
