59d0753c3e3ca59ed55e54cd68b05e763576f30d
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-11-05 20:59:09 -0500

    Loan page created

---
+++ src/Components/LoanApp.jsx
@@ -0,0 +1,19 @@
+const LoanPage = () => {
+  return (
+    <div>
+      <h1>Loan Aplication</h1>
+      <h4>Fillt out to Form to Aplly</h4>
+      <div>
+        <form>
+          <input>name</input>
+          <input>adress</input>
+          <input>income</input>
+          <input>total debt</input>
+          <input>Loan Amount</input>
+        </form>
+        <button>submit</button>
+      </div>
+    </div>
+  );
+};
+export default LoanPage;
