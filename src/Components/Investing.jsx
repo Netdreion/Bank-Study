d584e13e48c77f25fe414a04510dfa56a7ae5efd
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-11-07 23:29:33 -0500

    table rewised

--- src/Components/Investing.jsx
+++ src/Components/Investing.jsx
@@ -17,6 +17,7 @@ const Investing = () => {
         if (response.ok) {
           const data = await response.json();
           setReceivedData(data);
+          console.log(receivedData);
         } else {
           console.error(`Failed to fetch data. Status: ${response.status}`);
         }
@@ -40,23 +41,32 @@ const Investing = () => {
 
       {show && receivedData && (
         <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Stock</th>
-                <th>Price</th>
-                <th>daily change</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Apple</td>
-                <td>{receivedData.c}</td>
-                <td>{receivedData.d}</td>
-                <td>{/*receivedData*/}</td>
-              </tr>
-            </tbody>
-          </table>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Stock</th>
+                  <th>Price</th>
+                  <th>daily change</th>
+                  <th>% change</th>
+                  <th>highest price</th>
+                  <th>lowest price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Apple</td>
+                  <td>{receivedData.c}</td>
+                  <td>{receivedData.d}</td>
+                  <td>{receivedData.dp}</td>
+                  <td>{receivedData.h}</td>
+                  <td>{receivedData.l}</td>
+
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </div>
@@ -64,3 +74,18 @@ const Investing = () => {
 };
 
 export default Investing;
+
+/*receivedData c: 181.82
+d: This might refer to the daily change in the stock's price. It shows how much the stock price has changed during the current trading day.
+
+dp: This could be the percentage change in the stock's price for the day. It represents the percentage by which the stock's price has increased or decreased during the current trading day.
+
+h: This is the highest price the stock reached during the trading day.
+
+l: This is the lowest price the stock reached during the trading day.
+
+o: This could represent the opening price of the stock for the current trading day.
+
+pc: This might refer to the previous day's closing price of the stock.
+
+*/
