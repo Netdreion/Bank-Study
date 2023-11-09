7b4d130560e5387991d071829479c4271211e3b4
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-11-08 22:14:19 -0500

    dashboard rewised

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -73,6 +73,7 @@ const DashBoard = () => {
 
   return (
     <div className="dashboard-container">
+      <h1>finovance</h1>
       <div className="login-container">
         <Login
           setLoginForm={setLoginForm}
