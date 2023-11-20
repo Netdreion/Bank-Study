81e07a02bbeb2573065247d8426ed3e2bae28f2f
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-11-19 21:11:47 -0500

    loan comp reised

--- src/Components/LoanApp.jsx
+++ src/Components/LoanApp.jsx
@@ -1,32 +1,61 @@
+import { useState } from "react";
+
 const LoanPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    adress: "",
+    income: "",
+    debt: "",
+    name: "",
+    loanAmount: "",
+  });
+  const [collectFormData, setCollectFormData] = useState([]);
 
-    const[formData,setFormData]=({
-        name:'',
-        adress:"",
-        income:'',
-        debt:"",name:'',
-        LoanAmount:"",
-        
-    })
+  const handleSubmit = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-
-
+  const handleClick = () => {};
+  setCollectFormData([...collectFormData, { ...formData }]);
   return (
     <div>
       <h1>Loan Aplication</h1>
       <h4>Fillt out to Form to Aplly</h4>
       <div>
-        <form onSubmit={(e)=>e.tarrget.value}>
-          <input value={} name={}  placeholder="name here"> name</input>
-          <input value={} name={}  placeholder="adress here" >adress</input>
-          <input value={} name={}  placeholder="income here" >income</input>
-          <input value={} name={}  placeholder="debt here" >total debt</input>
-          <input value={} name={}  placeholder="Loan Amount here" >Loan Amount</input>
+        <form onSubmit={handleSubmit}>
+          <input value={formData.name} name="name" placeholder="name here">
+            {" "}
+            name
+          </input>
+          <input
+            value={formData.adress}
+            name="adress"
+            placeholder="adress here"
+          >
+            adress
+          </input>
+          <input
+            value={formData.income}
+            name="income"
+            placeholder="income here"
+          >
+            income
+          </input>
+          <input value={formData.debt} name="debt" placeholder="debt here">
+            total debt
+          </input>
+          <input
+            value={formData.loanAmount}
+            name="name"
+            placeholder="Loan Amount here"
+          >
+            Loan Amount
+          </input>
         </form>
-        <button>submit</button>
+        <button onClick={handleClick}>submit</button>
       </div>
     </div>
   );
 };
 export default LoanPage;
-
