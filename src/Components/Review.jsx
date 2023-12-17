ed8ca00e9db7901c27382477a51797730ba7669a
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-12-16 23:31:04 -0500

    timers time revised

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -29,7 +29,7 @@ const Review = () => {
   useEffect(() => {
     const timer = setInterval(() => {
       randomBtn();
-    }, [5000]);
+    }, [3000]);
 
     return () => clearInterval(timer);
   }, [people, index]);
