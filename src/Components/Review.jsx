3b2eab8fd548c17cb887b489cc5e39c2852ef766
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-12-17 23:10:08 -0500

    react-icons installed

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import people from "./ReviewData";
+import { FaStar } from "react-icons/fa";
 
 const Review = () => {
   const [index, setIndex] = useState(0);
