cc835692b01469c2d9b0e9fd0af20c46aaadeb46
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-12-28 23:09:46 -0500

    rewied and renewed

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -2,17 +2,9 @@ import { useState, useEffect } from "react";
 import people from "./ReviewData";
 import { FaStar } from "react-icons/fa";
 
-const storageSetUp = () => {
-  const setUp = JSON.parse(localStorage.getItem("list")) || [];
-
-  return setUp;
-};
-
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, review, star } = people[index];
-  const [addReview, setAddReview] = useState("");
-  const [reviewList, setReviewList] = useState([storageSetUp]);
 
   const nextBtn = () => {
     if (index < 4) {
@@ -43,17 +35,6 @@ const Review = () => {
     return () => clearInterval(timer);
   }, [people, index]);
 
-  const handleChange = (e) => {
-    setAddReview(e.target.value);
-  };
-  const handleClick = () => {
-    setReviewList([...reviewList, addReview]);
-  };
-
-  useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(reviewList));
-  }, [reviewList]);
-
   return (
     <div>
       <h2>Reviews</h2>
@@ -71,30 +52,6 @@ const Review = () => {
       <button onClick={() => nextBtn()}>next</button>
       <button onClick={() => prevBtn()}>prev</button>
       <button onClick={() => randomBtn()}>random</button>
-
-      <div>
-        <h4>add review</h4>
-
-        <form>
-          <section>
-            <input
-              type="text"
-              value={addReview}
-              placeholder="add review"
-              onChange={handleChange}
-            ></input>
-            <input>name</input>
-            <input type="number">star</input>
-            <button onClick={handleClick}>add</button>
-            <article>
-              {reviewList.map((item, reviewIndex) => {
-                return <p key={reviewIndex}>{item}</p>;
-              })}
-            </article>
-            <button onClick={() => setReviewList([])}>clear</button>
-          </section>
-        </form>
-      </div>
     </div>
   );
 };
